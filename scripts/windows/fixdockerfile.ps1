89e948f8143fdaf07c317e3634ba6b9cd7297c41
Andres Rios Tascon <ariostas@gmail.com>
2021-11-10 12:14:35 -0500

    Fixed Windows installation script

--- scripts/windows/fixdockerfile.ps1
+++ scripts/windows/fixdockerfile.ps1
@@ -2,3 +2,4 @@
 
 (Get-content ..\..\Dockerfile) | %{$_ -replace "USER cytools","# Line removed for Windows Dockerfile"} | Set-content ..\..\Dockerfile
 (Get-content ..\..\Dockerfile) | %{$_ -replace "/home/cytools/.local/lib/python3.7/site-packages","/usr/local/lib/python3.7/dist-packages"} | Set-content ..\..\Dockerfile
+(Get-content ..\Dockerfile) | %{$_ -replace "CMD jupyter lab --ip 0.0.0.0 --port 2875 --no-browser","CMD jupyter lab --ip 0.0.0.0 --port 2875 --allow-root --no-browser"} | Set-content ..\Dockerfile
