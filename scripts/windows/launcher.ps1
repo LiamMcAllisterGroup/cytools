9264c0e0c04664a1c698f77a8b7e9ee59403d8a6
Andres Rios Tascon <ariostas@gmail.com>
2021-11-22 15:42:19 -0500

    Bumped to v0.1.1

--- scripts/windows/launcher.ps1
+++ scripts/windows/launcher.ps1
@@ -38,7 +38,7 @@ $banner=@"
  ░░█████████     █████       █████   ░░██████ ░░██████  █████ ██████
   ░░░░░░░░░     ░░░░░       ░░░░░     ░░░░░░   ░░░░░░  ░░░░░ ░░░░░░
 
-        Developed by Liam McAllister's Group | Version 0.1.0
+        Developed by Liam McAllister's Group | Version 0.1.1
              https://cytools.liammcallistergroup.com
 
 "@
