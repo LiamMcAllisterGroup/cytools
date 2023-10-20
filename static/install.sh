927120f7cad79784e8673006498ccbb70a3c4f29
Nate MacFadden <natemacfadden@gmail.com>
2023-10-20 18:05:51 -0400

    optional package flag

--- static/install.sh
+++ static/install.sh
@@ -4,7 +4,20 @@ cd /tmp/
 tmp_dir="cytools-update-$RANDOM"
 mkdir $tmp_dir
 cd $tmp_dir
-curl -s https://api.github.com/repos/LiamMcAllisterGroup/cytools/tags | grep "tarball_url" | grep -Eo 'https://[^\"]*' | sed -n '1p' | xargs wget -q -O - | tar -xz --strip-components 1
-make install
+
+# Fetch the latest release tarball
+curl -s https://api.github.com/repos/LiamMcAllisterGroup/cytools/tags \
+| grep "tarball_url" \
+| grep -Eo 'https://[^\"]*' \
+| sed -n '1p' \
+| xargs wget -q -O - \
+| tar -xz --strip-components 1
+
+# Default value of whether to install optional packages
+: "${OPTIONAL_PKGS:=0}"
+
+# Install
+make install OPTIONAL_PKGS=$OPTIONAL_PKGS
+
 cd /tmp/
 rm -rf cytools-update-*
