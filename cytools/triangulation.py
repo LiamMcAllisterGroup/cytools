9502280a5aaa23a8b4f2ba7423fab130bec655ea
Nate MacFadden <natemacfadden@gmail.com>
2024-01-21 22:59:32 -0500

    tuple labels
    deals with mutability concerns

--- cytools/triangulation.py
+++ cytools/triangulation.py
@@ -199,7 +199,7 @@ class Triangulation:
 
         # points
         # (ordered to match poly.label ordering...)
-        self._labels = [label for label in poly.labels if label in pts]
+        self._labels = tuple([label for label in poly.labels if label in pts])
         self._labels2inds = {v:i for i,v in enumerate(self._labels)}
 
         # dimension
@@ -744,7 +744,6 @@ class Triangulation:
         ```
         """
         return self.get_toric_variety().get_cy(nef_partition)
-
     # aliases
     compute_cy = get_cy
     cy = get_cy
