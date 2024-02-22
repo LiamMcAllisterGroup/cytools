392b376a11c914d48abb50131a2672db42207ad1
Nate MacFadden <natemacfadden@gmail.com>
2024-02-22 12:15:22 -0500

    formally update the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="cytools",
-    version="1.1.0",
+    version="1.1.1",
     author="Liam McAllister Group",
     author_email="",
     description="A software package for analyzing Calabi-Yau hypersurfaces in toric varieties.",
