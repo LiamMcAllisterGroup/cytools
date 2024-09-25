ebbf21141e9a0044b0f8314477ed6a94a8344854
Nate MacFadden <natemacfadden@gmail.com>
2024-09-25 10:03:21 -0400

    update the version

--- src/cytools/__init__.py
+++ src/cytools/__init__.py
@@ -20,7 +20,7 @@ from cytools.cone import Cone
 from cytools.utils import read_polytopes, fetch_polytopes
 
 # Latest version
-version = "1.2.3"
+version = "1.2.4"
 versions_with_serious_bugs = []
 
 # Check for more recent versions of CYTools
