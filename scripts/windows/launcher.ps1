67f435aa560155352e25792ce66f03829fb6c3e1
Nate MacFadden <natemacfadden@gmail.com>
2024-10-25 14:02:34 -0400

    Formally update the windows version
    I've been missing this commit in the past - my bad!

--- scripts/windows/launcher.ps1
+++ scripts/windows/launcher.ps1
@@ -39,7 +39,7 @@ $banner=@"
   ░░░░░░░░░     ░░░░░       ░░░░░     ░░░░░░   ░░░░░░  ░░░░░ ░░░░░░
 
                 Developed by Liam McAllister's Group
-                  https://cy.tools | Version 1.0.4
+                  https://cy.tools | Version 1.2.5
 
 "@
 
