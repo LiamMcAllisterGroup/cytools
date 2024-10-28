b4d7fd9769832f9f3b528c17130a3ca7bc4b3ce7
Nate MacFadden <natemacfadden@gmail.com>
2024-10-28 13:21:29 -0400

    Update to version 1.2.6

--- src/cytools/__init__.py
+++ src/cytools/__init__.py
@@ -20,7 +20,7 @@ from cytools.cone import Cone
 from cytools.utils import read_polytopes, fetch_polytopes
 
 # Latest version
-version = "1.2.5"
+version = "1.2.6"
 versions_with_serious_bugs = []
 
 
